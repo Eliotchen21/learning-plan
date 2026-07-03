b30b9c32f503e5f3c505ceb0094da31b2210fdbb
Eliotchen21 <eliotchen21@gmail.com>
2026-07-03 13:10:06 +0800

    debug

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+import { firebaseConfig } from "./config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
