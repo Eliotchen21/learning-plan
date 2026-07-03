058837a340f03c912a017bf5bdaa27285bab7e4b
Eliotchen21 <eliotchen21@gmail.com>
2026-07-03 13:13:30 +0800

    debug

--- firebase.js
+++ firebase.js
@@ -1,6 +1,15 @@
-import { firebaseConfig } from "./config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCL0OdorkZ1Y-lAGxh_J5MxRZMgFplZMjo",
+    authDomain: "test-2430c.firebaseapp.com",
+    projectId: "test-2430c",
+    storageBucket: "test-2430c.firebasestorage.app",
+    messagingSenderId: "78873597685",
+    appId: "1:78873597685:web:ea25cb60c1eac7af7217bc",
+    measurementId: "G-RNBHVTQQNR"
+}
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
